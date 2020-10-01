f9075f2f9ba5b08ef0338250256197dd67977efe
Scott van den Broek <you@example.com>
2020-10-01 14:58:53 +0200

    02-MemesGifs toegevoegd

---
+++ 03-MemesGifs/maak_meme.py
@@ -0,0 +1,35 @@
+from PIL import Image, ImageFont, ImageDraw
+
+achtergrond = Image.open("meme_background.jpg")
+
+achtergrond.show()
+
+breedte = achtergrond.width
+hoogte = achtergrond.height
+
+lettertype = ImageFont.truetype("impact.ttf", 40)
+
+# Vraag aan de ImageDraw module om een tekengebied te maken op de achtergrond afbeelding
+tekengebied = ImageDraw.Draw(achtergrond)
+
+# Tekst schrijven
+tekst = "Coding in Python\nNo problemo!"
+
+# Bereken de breedte en hoogte van de tekst
+tekst_breedte, tekst_hoogte = tekengebied.textsize(tekst, font=lettertype) 
+print("tekstbreedte=" + str(tekst_breedte) + ", tekst_hoogte=" + str(tekst_hoogte))
+
+# Tekst positie berekenen
+tekst_x = (breedte - tekst_breedte) / 2
+tekst_y = (hoogte - tekst_hoogte) / 2 
+
+tekengebied.multiline_text((tekst_x-2, tekst_y-2), tekst, font=lettertype, fill=(255,255,255))
+
+tekengebied.multiline_text((tekst_x,tekst_y), tekst, font=lettertype, fill=(0,0,0))
+achtergrond.show()
+
+
+
+# En opslaan onder een andere naam
+achtergrond.save("meme_met_tekst.jpg")
+
